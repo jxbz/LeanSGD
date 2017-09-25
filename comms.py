a34812fff3684846a3a201d94420f68c99356a1f
Scott Sievert <sievert.scott@icloud.com>
2017-09-25 10:13:21 -0500

    Encoding complete SVD for >=2 dimensions (except for grads with name 'convShortcut' in name... BUG

--- comms.py
+++ comms.py
@@ -1,5 +1,5 @@
-import torch
 import numpy as np
+import torch
 # TODO:
 # *
 #   * time / profile
@@ -10,6 +10,7 @@ storage = {}
 verbose = True
 comm_bytes = 0
 
+
 def _get_size(d, verbose=True):
     print(d)
     sizes = [np.prod(v['size']) for k, v in d.items()]
@@ -19,6 +20,24 @@ def _get_size(d, verbose=True):
         print("storage takes up {mb}MB".format(mb=n_bytes / 1024**2))
     return n_bytes
 
+
+def _resize_tensor(x):
+    """
+    x.shape > 2
+
+    If x.shape = (a, b, *c), assumed that each one of (a, b) pairs has relevant information in c.
+    """
+    size = x.size()
+    #  print(tuple(size))
+    if all([s == 1 for s in size[2:]]):
+        # if (a, b, 1, 1)
+        return x.view(size[0], size[1])
+    # each of (a, b) has related features
+    x = x.view(size[0], size[1], -1)
+    # stack those related features into a tall matrix
+    return x.view(-1, size[0]*size[1])
+
+
 def encode(name=None):
     assert name is not None, "name cannot be none"
 
@@ -27,30 +46,30 @@ def encode(name=None):
             print("set keys =", storage.keys())
 
         storage[name] = storage.get(name, {})
-        storage[name]['size'] = grad.data.size()
+        grad = grad.data
+        storage[name]['size'] = grad.size()
+        size = grad.size()
 
-        if storage[name].get('zero', True):
-            storage[name]['zero'] = False
-            if len(grad.size()) == 2:
-                print(f'Encoding SVD of {name}')
+        if len(grad.size()) > 2:
+            grad = _resize_tensor(grad)
+            storage[name]['reshaped'] = True
+
+        # relative error when decoding names with 'shortcut' in name is >=30
+        take_svd = len(grad.size()) == 2 and 'shortcut' not in name.lower()
+        if storage[name].get('initialize', True):
+            storage[name]['initialize'] = False
+            storage[name]['grad'] = grad
+
+            if take_svd:
                 storage[name]['encode'] = True
-                (u, s, v) = torch.svd(grad.data)
-                storage[name]['svd'] = {'u':u, 's':s, 'v':v}
-                storage[name]['grad'] = grad.data
-            else:
-                storage[name]['grad'] = grad.data
+                (u, s, v) = torch.svd(grad)
+                storage[name]['svd'] = {'u': u, 's': s, 'v': v}
         else:
-            if len(grad.size()) == 2:
-                print(f'Adding to encoding SVD of {name}')
-                (u, s, v) = torch.svd(grad.data)
-
-                for key, value in {'u':u, 's':s, 'v':v}.items():
+            storage[name]['grad'] += grad
+            if take_svd:
+                (u, s, v) = torch.svd(grad)
+                for key, value in {'u': u, 's': s, 'v': v}.items():
                     storage[name]['svd'][key] += value
-                storage[name]['grad'] += grad.data
-            else:
-                storage[name]['grad'] += grad.data
-
-        #  storage[name]['grad'] += grad.data
 
     return hook
 
@@ -59,19 +78,21 @@ def decode(name, verbose=False):
     """
     Returns gradient as torch Tensor
     """
-    #  _get_size(storage, verbose=verbose)
-    storage[name]['zero'] = True
+    storage[name]['initialize'] = True
 
     if verbose:
         print("get keys =", storage.keys())
     if not storage[name].get('encode', False):
         return storage[name]['grad']
 
-    print(f'Decoding SVD of {name}')
     u, s, v = [storage[name]['svd'][k] for k in ['u', 's', 'v']]
-    del storage[name]['svd']
     grad = storage[name]['grad']
     grad_approx = u @ torch.diag(s) @ v.t()
 
+    if storage[name].get('reshaped', False):
+        grad_approx = grad_approx.view(storage[name]['size'])
+
     rel_error = torch.norm(grad_approx - grad) / torch.norm(grad)
+    del storage[name]['svd']
+    del storage[name]['grad']
     return grad_approx
