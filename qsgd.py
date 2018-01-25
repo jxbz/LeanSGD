47342371df85218632227100ae48273d22d4aced
Scott Sievert <sievert.scott@icloud.com>
2018-01-25 16:24:03 -0600

    Moving qsgd to CPU

--- qsgd.py
+++ qsgd.py
@@ -7,40 +7,34 @@ import time
 
 def encode(v, **kwargs):
     norm = torch.norm(v)
-    w = v.view(-1)
+    w = v.view(-1).numpy()
 
-    t = [time.time()]
-    signs = torch.sign(w).int()
-    probs = torch.abs(w) / norm
-    mask = torch.distributions.Bernoulli(probs).sample().byte()
-    t += [time.time()]
-    idx = torch.arange(0, len(w))
-    t += [time.time()]
-    if v.is_cuda:
-        idx = idx.cuda()
-        mask = mask.cuda()
-    t += [time.time()]
+    signs = np.sign(w).astype('int') #.int()
+    probs = np.abs(w) / norm
+    mask = stats.bernoulli.rvs(probs).astype('bool')
+    mask[0] = 1
+    idx = np.arange(len(w))
 
-    selected = torch.masked_select(idx, mask).long()
-    signs = torch.masked_select(signs, mask)
-    t += [time.time()]
-    data = {'masking_time': t[-1] - t[-2], 'gen_mask_time': t[1] - t[0],
-            'to_gpu_time': t[-2] - t[-3]}
-    return {'signs': signs, 'size': v.size(), 'selected': selected,
-            'norm': norm}#, data
+    selected = idx[mask].astype('int16')
+    signs = signs[mask].astype('int8')
+    data = {}
+    code = {'signs': signs, 'size': v.size(), 'selected': selected,
+            'norm': norm}
+    if kwargs.pop('timings', False):
+        return code, data
+    return code
 
 
 def decode(code, cuda=False):
-    v = torch.zeros(code['size'])
-    signs = code['signs']
-    print("QSGD cuda =", cuda)
+    v = np.zeros(code['size'])
+    signs = np.array(code['signs'], dtype='int8')
+    selected = np.array(code['selected'], dtype='int16')
+    if len(code['selected']) > 0:
+        r = code['norm'] * signs.flat[:]
+        v.flat[selected] = r
+    v = torch.Tensor(v)
     if cuda:
         v = v.cuda()
-        signs = signs.cuda()
-    flat = v.view(-1)
-    if len(code['selected']) > 0:
-        r = code['norm'] * signs.float()
-        flat[code['selected']] = r
     return v
 
 if __name__ == "__main__":
